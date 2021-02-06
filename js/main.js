815dda2e3fafe413276825aad9f64a89210fc8b6
Oleksangr Ilchuck <sawalviv452@gmail.com>
2021-02-06 02:31:34 +0200

    finish

--- js/main.js
+++ js/main.js
@@ -5,6 +5,14 @@ $(function(){
         dots: true,
         autoplay: true,
         fade: true,
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    dots: false,
+                }
+            },
+        ]
         
     });
 
@@ -13,7 +21,29 @@ $(function(){
         dots: true,
         slidesToShow: 4,
         slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1141,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 846,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 585,
+                settings: {
+                    slidesToShow: 1,
+                }
+            },
+        ]
 
     });
-
+    $('.menu__btn').on('click', function(){
+        $('.menu__list').toggleClass('menu__list--active');
+    });
 });
